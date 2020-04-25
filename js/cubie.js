14fb666a04260d5813d2ff1b8b3b4f9238c0dadd
ricsonl <ricsonluizv@gmail.com>
2020-04-25 17:00:58 -0300

    translation method change

--- js/cubie.js
+++ js/cubie.js
@@ -1,57 +1,24 @@
 class Cubie {
-    pos = undefined;
-    len = undefined;
-    constructor(x, y, z, len){
-        this.pos = createVector(x, y, z);
-        this.len = len;
+    matrix = undefined;
+    highlight = undefined;
+
+    constructor(m){
+        this.matrix = m;
+        this.highlight = false;
     }
 
     show(){
+        fill(255);
+        if(this.highlight){
+            fill(255, 0, 0);
+        }
         stroke(0);
         strokeWeight(5);
 
         push();
-
-        translate(this.pos.x, this.pos.y, this.pos.z);
-        const r = len*0.5;
-        
-        //Z
-        fill(colors.bck);
-        quad(-r, -r, -r,
-              r, -r, -r,
-              r,  r, -r,
-             -r,  r, -r);
-        fill(colors.frt);
-        quad(-r, -r, r,
-              r, -r, r,
-              r,  r, r,
-             -r,  r, r);
-
-        
-        //Y
-        fill(colors.upp);
-        quad(-r, -r, -r,
-              r, -r, -r,
-              r, -r,  r,
-             -r, -r,  r);
-        fill(colors.dwn);
-        quad(-r,  r, -r,
-              r,  r, -r,
-              r,  r,  r,
-             -r,  r,  r);
-        
-        //X
-        fill(colors.lft);
-        quad(-r, -r, -r,
-             -r,  r, -r,
-             -r,  r,  r,
-             -r, -r,  r);
-        fill(colors.rgt);
-        quad( r, -r, -r,
-              r,  r, -r,
-              r,  r,  r,
-              r, -r,  r);
-        
+        applyMatrix(...this.matrix);
+        box(1);
+        resetMatrix();
         pop(); 
     }
 }
--- js/index.js
+++ js/index.js
@@ -3,39 +3,40 @@ const len = 50;
 const offset = (dim-1)*len*0.5;
 
 var cube = [];
+
 const colors = {
-    upp: '#009b48', dwn: '#0046ad',
-    lft: '#b71234', rgt: '#ff5800',
-    frt: '#ffffff', bck: '#ffd500'
+    upp: '#009b00', dwn: '#0046ad',
+    lft: '#e32000', rgt: '#ff5a00',
+    frt: '#ffffff', bck: '#ffd300'
 };
 
 function setup() {
     createCanvas(500, 500, WEBGL);
-    for(let i=0; i < dim; i++){
-        cube[i] = [];
-
-        for(let j=0; j < dim; j++){
-            cube[i][j] = [];
-
-            for(let k=0; k < dim; k++){
-                cube[i][j][k] = new Cubie(i*len-offset, j*len-offset, k*len-offset, len);
+    var index = 0;
+    for(let x = -1; x <= 1; x++){
+        for(let y = -1; y <= 1; y++){
+            for(let z = -1; z <= 1; z++){
+                mat = [1, 0, 0, 0,
+                       0, 1, 0, 0,
+                       0, 0, 1, 0,
+                       x, y, z, 1];
+                cube[index] = new Cubie(mat);
+                index++;
             }
         }
     }
+    cube[0].highlight = true;
 }
 
 function draw() {
     clear();
     background('#333');
+    scale(50);
     rotX = map(mouseY, height, 0, -3.2, 3.2);
     rotY = map(mouseX, width, 0, 3.2, -3.2);
     rotateX(rotX);
     rotateY(rotY);
-    for (let i = 0; i < dim; i++) {
-        for (let j = 0; j < dim; j++) {
-            for (let k = 0; k < dim; k++) {
-                cube[i][j][k].show();
-            }
-        }
+    for (let i = 0; i < cube.length; i++) {
+        cube[i].show();
     }
 }
